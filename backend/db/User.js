acd8be8f274781510c849c231195ebaa7d3d9275
pravin <pravin@piggyride.in>
2023-02-20 23:08:54 +0530

    local storage

---
+++ backend/db/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name : String,
+    password : String,
+    email : String
+});
+
+module.exports = mongoose.model("users", userSchema);
+
+//parameter users is matched with database collection name 
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,33 @@
-const express = require("express");
-const dbConnect = require('./mongodb');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require ('cors');
+mongoose.set('strictQuery', false);
+require('./db/config');
+const User = require('./db/User');
 const app = express();
 
-app.get("/", async (req, resp)=>{
-    resp.send("working");
-    data = await data.find().toArray();
-    console.log(data)
+app.use(cors());
+app.use(express.json());
 
+app.post("/register", async (req, resp)=>{
+let user = new User(req.body);
+let result = await user.save();
+
+resp.send(result);// getting form or boday data in nodejs
 })
 
-app.listen(5000);
+app.listen(8000);
+
+
+// const dbConnect = async () => {
+// 	mongoose.connect('mongodb://localhost:27017/ecommerce');
+// 	const PorductSchema = new mongoose.Schema({});
+// 	const product = mongoose.model('users', PorductSchema);
+// 	const data = await product.find();
+// 	console.warn(data);
+// };
+
+// app.get("/", async (req, resp)=>{
+//     resp.send("working");
+
+// })
